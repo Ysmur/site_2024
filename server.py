5ca37fdc5f2cf18998a2998f40cf2e4201a956b4
Ysmur <ysmur@yandex.ru>
2025-02-09 21:44:56 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,30 @@
 import os
 from flask import Flask, render_template
+from flask_restful import Resource, Api, reqparse
+import requests
+from bs4 import BeautifulSoup
+
+class Goods(Resource):
+  def get(self, barcode):
+      url = f'https://barcode-list.ru/barcode/RU/%D0%9F%D0%BE%D0%B8%D1%81%D0%BA.htm?barcode={barcode}'
+      response = requests.get(url)
+      print(response)
+      bs = BeautifulSoup(response.text, 'html.parser')
+      temp = bs.find('h1', 'pageTitle')
+      return temp.text.split(' - ')[0]
 
 app = Flask(__name__)
+api = Api(app)
+api.add_resource(Goods, '/goods/<barcode>')
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+  
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
 
 if __name__ == '__main__':
